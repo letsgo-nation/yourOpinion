174acd71e1e5d4da3816042db62e414b85fe3e4b
chae0 <limchae098@gmail.com>
2023-07-17 18:18:10 +0900

    Init Commit

---
+++ src/test/java/com/example/youropinion/YourOpinionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youropinion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourOpinionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
