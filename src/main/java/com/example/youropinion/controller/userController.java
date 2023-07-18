97298d4f80f330d194ee22de515174a9ab4d5f66
MSM <qw12345611@naver.com>
2023-07-18 21:54:37 +0900

    LoginPage css commit

--- src/main/java/com/example/youropinion/controller/userController.java
+++ src/main/java/com/example/youropinion/controller/userController.java
@@ -7,8 +7,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-@RestController
+
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class userController {
