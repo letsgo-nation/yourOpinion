5692d32ce99349216154112970eb1a21e6753d64
LEEMINWOO <mw7328@naver.com>
2023-07-20 20:32:00 +0900

    상세페이지 댓글 작업 중

--- src/main/java/com/example/youropinion/controller/CommentController.java
+++ src/main/java/com/example/youropinion/controller/CommentController.java
@@ -27,6 +27,8 @@ CommentController {
             @Valid @RequestBody CommentRequestDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
         this.tokenValidate(userDetails);
+
+        System.out.println("userDetails.getUser() = " + userDetails.getUser());
         return commentService.createComment(id,requestDto, userDetails.getUser());
     }
 
